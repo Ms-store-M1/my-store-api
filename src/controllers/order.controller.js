46eb2a2b1006c1618433055217ab1cf80b7bdb48
yi3444 <yasmine.ibrahimi@ynov.com>
2024-03-21 16:20:17 +0100

    update order route

--- src/controllers/order.controller.js
+++ src/controllers/order.controller.js
@@ -6,42 +6,16 @@ const createOrder = async (req, res) => {
   const { deliveryMode, deliveryAddress, paymentDetails } = req.body;
 
   try {
-    let orderUser = null;
-
-    // si l'user est connecté
-    if (userId) {
-      orderUser = await prisma.user.findUnique({
-        where: { id: parseInt(userId, 10) },
-      });
-    }
-
-    // si l'user n'est pas connecté
-    if (!orderUser) {
-      // On verifie si l'user est inscrit mais non connecté
-      const { email } = req.body;
-      const userExists = await prisma.user.findFirst({
-        where: { mail: email },
-      });
-
-      if (userExists) {
-        // Redirection  vers la page de connexion s'il l'utilisateur est inscrit
-        return res.status(400).json({ message: "Veuillez vous connecter pour passer une commande" });
-      } else {
-        // Si l'user est non inscrit, redirection vers la page d'inscription
-        return res.status(400).json({ message: "Veuillez vous inscrire pour passer une commande" });
-      }
-    }
-
-    // Récupérer le panier de l'utilisateur
+    // Récupérer le panier 
     const cartItems = await prisma.cart.findMany({
       where: { userId: parseInt(userId, 10) },
       include: { product: true },
     });
 
-    // Création de la commande 
+    // Création de la commande
     const order = await prisma.order.create({
       data: {
-        userId: orderUser.id,
+        userId: parseInt(userId, 10),
         products: { create: cartItems.map(item => ({ productId: item.productId, quantity: item.quantity })) },
         deliveryMode,
         deliveryAddress: deliveryMode === "livraison à domicile" ? deliveryAddress : null,
@@ -49,8 +23,8 @@ const createOrder = async (req, res) => {
       },
     });
 
-    // Vider le panier de l'utilisateur
-    await prisma.cart.deleteMany({ where: { userId: orderUser.id } });
+    // Vider le panier 
+    await prisma.cart.deleteMany({ where: { userId: parseInt(userId, 10) } });
 
     res.json(order);
   } catch (error) {
--- src/routes/order.route.js
+++ src/routes/order.route.js
@@ -2,6 +2,6 @@ const express = require("express");
 const router = express.Router();
 const orderController = require("../controllers/order.controller");
 
-router.post("/create", orderController.createOrder);
+router.post("/createorder", orderController.createOrder);
 
 module.exports = router;
