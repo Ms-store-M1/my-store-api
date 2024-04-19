528a09e3a972438158e1b32c4902b842105daeab
AlexismlaWeb <102291173+AlexismlaWeb@users.noreply.github.com>
2024-04-19 12:14:28 +0200

    Update app.js

--- src/app.js
+++ src/app.js
@@ -20,7 +20,7 @@ app.use(express.urlencoded({ extended: true }));
 // cors
 app.use(
     cors({
-        origin: "http://localhost:3000",
+        origin: "https://my-store-api-uq4g.onrender.com",
     })
 );
 
